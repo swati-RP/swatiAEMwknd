8b94858ed559f687705afc7d57448cdc8e5c1e92
Swati mishra <swatimishra@rightpoint.com>
2023-06-21 21:20:08 +0530

    column code

--- core/src/main/java/com/adobe/aem/guides/wknd/core/models/Column.java
+++ core/src/main/java/com/adobe/aem/guides/wknd/core/models/Column.java
@@ -1,19 +1,19 @@
-package com.adobe.aem.guides.wknd.core.models;
+    package com.adobe.aem.guides.wknd.core.models;
 
-import java.util.List;
-import java.util.Map;
+    import java.util.List;
+    import java.util.Map;
 
-public interface Column {
+    public interface Column {
 
-    String getTitle();
-    String getDescription();
-    List<String> getFeature();
-    String getPhoneNumber();
-    String getCtaText();
-    String getCtaLink();
-    String getImageCopy();
-    String getButton();
-    String getSubtitle();
-    List<Map<String, String>> getMultiDataMap(); 
-}
+        String getTitle();
+        String getDescription();
+        List<String> getFeature();
+        String getPhoneNumber();
+        String getCtaText();
+        String getCtaLink();
+        String getImageCopy();
+        String getButton();
+        String getSubtitle();
+        List<Map<String, String>> getMultiDataMap(); 
+    }
 
--- core/src/main/java/com/adobe/aem/guides/wknd/core/models/impl/ColumnImpl.java
+++ core/src/main/java/com/adobe/aem/guides/wknd/core/models/impl/ColumnImpl.java
@@ -1,148 +1,148 @@
-package com.adobe.aem.guides.wknd.core.models.impl;
+    package com.adobe.aem.guides.wknd.core.models.impl;
 
-import com.adobe.aem.guides.wknd.core.models.Column;
+    import com.adobe.aem.guides.wknd.core.models.Column;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+    import java.util.ArrayList;
+    import java.util.List;
+    import java.util.Map;
 
-import javax.annotation.PostConstruct;
-import javax.inject.Named;
+    import javax.annotation.PostConstruct;
+    import javax.inject.Named;
 
-import org.apache.sling.api.SlingHttpServletRequest;
-import org.apache.sling.api.resource.Resource;
-import org.apache.sling.models.annotations.DefaultInjectionStrategy;
-import org.apache.sling.models.annotations.Model;
-import org.apache.sling.models.annotations.injectorspecific.ChildResource;
-import org.apache.sling.models.annotations.injectorspecific.Self;
-import org.apache.sling.models.annotations.injectorspecific.SlingObject;
-import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
+    import org.apache.sling.api.SlingHttpServletRequest;
+    import org.apache.sling.api.resource.Resource;
+    import org.apache.sling.models.annotations.DefaultInjectionStrategy;
+    import org.apache.sling.models.annotations.Model;
+    import org.apache.sling.models.annotations.injectorspecific.ChildResource;
+    import org.apache.sling.models.annotations.injectorspecific.Self;
+    import org.apache.sling.models.annotations.injectorspecific.SlingObject;
+    import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
 
-@Model(adaptables = { SlingHttpServletRequest.class }, adapters = { Column.class }, resourceType = {
-    ColumnImpl.RESOURCE_TYPE }, defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
-public class ColumnImpl implements Column
+    @Model(adaptables = { SlingHttpServletRequest.class }, adapters = { Column.class }, resourceType = {
+        ColumnImpl.RESOURCE_TYPE }, defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
+    public class ColumnImpl implements Column
 
-{
-    protected static final String RESOURCE_TYPE = "wknd/components/column";
-
-    @Self
-    private SlingHttpServletRequest request;
-
-    @SlingObject
-    Resource componentResource;
-
-    @ValueMapValue
-    private String title;
-
-    @ValueMapValue
-    private String subtitle;
-
-    @ValueMapValue
-    private String description;
-
-    @ChildResource
-    @Named("feature")
-    private Resource featureResource;
-
-
-
-    @ValueMapValue
-    private String phoneNumber;
-
-    @ValueMapValue
-    private String ctaText;
-
-    @ValueMapValue
-    private String ctaLink;
-
-    @ValueMapValue
-    private String imageCopy;
-
-    @ValueMapValue
-    private String button;
-
-    private List<String> feature;
-
-
-    @Override
-    public String getButton()
     {
-        return button;
-    }
+        protected static final String RESOURCE_TYPE = "wknd/components/column";
 
-    @Override
-    public String getSubtitle()
-    {
-        return subtitle;
-    }
+        @Self
+        private SlingHttpServletRequest request;
 
-    @Override
-    public String getTitle() {
-        
-        return title;
-    }
+        @SlingObject
+        Resource componentResource;
 
-    @Override
-    public String getDescription() {
-        
-        return description;
-    }
+        @ValueMapValue
+        private String title;
 
-    @Override
-    public List<String> getFeature()
-    {
-     return feature;
-    }
-    
-    @Override
-    public String getPhoneNumber() {
-        
-        return phoneNumber;
-    }
+        @ValueMapValue
+        private String subtitle;
 
-    @Override
-    public String getCtaText() {
-        
-        return ctaText;
-    }
+        @ValueMapValue
+        private String description;
 
-    @Override
-    public String getCtaLink() {
-       
-        return ctaLink; 
-    }
+        @ChildResource
+        @Named("feature")
+        private Resource featureResource;
 
-    @Override
-    public String getImageCopy() {
-        
-        return imageCopy;
-    }
- 
- 
-    private List<Map<String, String>> details = new ArrayList<>();
- 
-    public List<Map<String, String>> getMultiDataMap() {
- 
-        return details;
- 
-    }
- 
-    @PostConstruct
-    protected void init() {
-        if (featureResource != null) {
-            feature=new ArrayList<>();
-            for (Resource child : featureResource.getChildren()) {
-                     feature.add(child.getValueMap().get("text", String.class));
-            }
+
+
+        @ValueMapValue
+        private String phoneNumber;
+
+        @ValueMapValue
+        private String ctaText;
+
+        @ValueMapValue
+        private String ctaLink;
+
+        @ValueMapValue
+        private String imageCopy;
+
+        @ValueMapValue
+        private String button;
+
+        private List<String> feature;
+
+
+        @Override
+        public String getButton()
+        {
+            return button;
         }
- 
+
+        @Override
+        public String getSubtitle()
+        {
+            return subtitle;
+        }
+
+        @Override
+        public String getTitle() {
+            
+            return title;
+        }
+
+        @Override
+        public String getDescription() {
+            
+            return description;
+        }
+
+        @Override
+        public List<String> getFeature()
+        {
+        return feature;
+        }
+        
+        @Override
+        public String getPhoneNumber() {
+            
+            return phoneNumber;
+        }
+
+        @Override
+        public String getCtaText() {
+            
+            return ctaText;
+        }
+
+        @Override
+        public String getCtaLink() {
+        
+            return ctaLink; 
+        }
+
+        @Override
+        public String getImageCopy() {
+            
+            return imageCopy;
+        }
+    
+    
+        private List<Map<String, String>> details = new ArrayList<>();
+    
+        public List<Map<String, String>> getMultiDataMap() {
+    
+            return details;
+    
+        }
+    
+        @PostConstruct
+        protected void init() {
+            if (featureResource != null) {
+                feature=new ArrayList<>();
+                for (Resource child : featureResource.getChildren()) {
+                        feature.add(child.getValueMap().get("text", String.class));
+                }
+            }
+    
+        }
+    
     }
- 
-}
 
 
 
 
 
- 
- 
+    
+    
